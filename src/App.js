739e3976be334136d024bdfcff4aed92321f6c7b
Evans Ibok <evansibok@gmail.com>
2019-12-02 15:30:18 +0100

    created todolist and presentational structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React from 'react';
 
+import data from './components/TodoComponents/Todo'
+import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: data,
+      initialTodo: {
+        task: '',
+        id: Date.now(),
+        completed: false,
+      }
+    }
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  onInputChange(evt) {
+    this.setState({
+      initialTodo: {
+        task: evt.target.value,
+      }
+    });
+  }
+
+  addTodo(evt) {
+    evt.preventDefault();
+  }
+
+
   render() {
+    const { todos, initialTodo } = this.state;
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
+        {/* Submit Button */}
+        add Submit handler to add button
+
+        {/* 'Clear all' Button */}
+        add Delete handler to clear all button
+
+        <TodoList
+          todos={todos}
+        />
+        <TodoForm
+          initialTodo={initialTodo}
+          onInputChange={this.onInputChange}
+          addTodo={this.addTodo}
+        />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class TodoForm extends Component {
+	render() {
+		return (
+			<div>
+				<input
+					type="text"
+					placeholder="...todo"
+					name="enter_todo"
+					value={this.props.initialTodo.task}
+					onChange={this.props.onInputChange}
+				/>
+				{` `}
+				<button onClick={this.props.addTodo}>Add Todo</button>
+				{` `}
+				<button>Clear all</button>
+			</div>
+		)
+	}
+}
